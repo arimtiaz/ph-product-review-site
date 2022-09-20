1f65ad019f13766fa645c82eeb8c967c42b3583f
Abadur Rahman Imtiaz <83642378+arimtiaz@users.noreply.github.com>
2022-09-20 23:31:23 +0600

    Updating Datas

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import { Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import Header from './components/Header/Header';
+import { Helmet } from 'react-helmet';
 
 
 function App() {
   return (
     <div className="App">
-        <FontAwesomeIcon icon={faCoffee} />
-        <Button variant='primary'>Prince</Button>
+        <Helmet>
+          <title>Harley Reviews</title>
+          <meta name="description" content="Helmet application" />
+        </Helmet>
+        <Header></Header>
     </div>
   );
 }

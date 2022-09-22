71a8cf852d1d12b310426e91543effd0ffae8185
Abadur Rahman Imtiaz <83642378+arimtiaz@users.noreply.github.com>
2022-09-22 13:56:26 +0600

    Working on Fetch

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,44 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faVideo } from '@fortawesome/free-solid-svg-icons';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import './Home.css'
 
 const Home = () => {
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() =>{
+        fetch('reviews.json')
+        .then(res => res.json())
+        .then(data => setReviews(data))
+    },[])
+
+    const reviewPush = (reviews) =>{
+        console.log(reviews)
+    }
+
     return (
         <div className='home'>
             {/* Hero Section */}
-           <div className="home-texts">
-           <h1 className='mt-5'>Only Solid Reviews Of <br /> <b>Harley Reviews</b></h1>
-            <Button variant='warning' className='mt-3'>
-                <FontAwesomeIcon icon={faVideo} className='me-3'></FontAwesomeIcon>
-                Watch Video
-            </Button>
+            <div className="hero">
+                <div className="home-texts">
+                    <h1 className='mt-5'>Only Solid Reviews Of <br /> <b>Harley Reviews</b></h1>
+                     <Button variant='warning' className='mt-3'>
+                    <FontAwesomeIcon icon={faVideo} className='me-3'></FontAwesomeIcon>
+                        Watch Video
+                    </Button>
            </div>
-            <div className="home-img">
+                <div className="home-img">
                 <img src="/src/images/bike.png" alt="" />
-            </div>
+                </div>
+                </div>
             {/* Review Section */}
+                <div className="review-section">
+                    <div className="review-heading">
+                        <h1>What Our Community Say</h1>
+                        <button onClick={() => reviewPush(reviews)}>Dekho</button>
+                    </div>
+            </div>
         </div>
     );
 };
